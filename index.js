4ad1403656ae71c45b943f5f4a6238187bf300ba
Groot737 <giorgiquchuloria7@gmail.com>
2026-09-14 02:33:48 +0400

    made app compatible for vercel

--- index.js
+++ index.js
@@ -122,6 +122,10 @@ app.get('/health', (req, res) => {
     res.json({ status: 'OK' });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
